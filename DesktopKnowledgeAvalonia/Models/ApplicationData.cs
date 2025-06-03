564ce9a709b1ba12ef9b7221762257e4a08957ef
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-06-04 05:06:47 +0800

    增加考试状态支持

--- DesktopKnowledgeAvalonia/Models/ApplicationData.cs
+++ DesktopKnowledgeAvalonia/Models/ApplicationData.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using LibraryOpenKnowledge.Models;
 
 namespace DesktopKnowledgeAvalonia.Models;
 
 [Serializable]
 public class ApplicationData
 {
-    
+    public bool IsInExamination { get; set; } = false;
+    public Examination? CurrentExamination { get; set; } = null;
 }
