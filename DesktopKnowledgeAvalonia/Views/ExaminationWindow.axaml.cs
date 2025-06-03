ae7a3fba75786ecfc60b8c1696d76f40c02f13a9
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-06-04 01:35:36 +0800

    初始化窗体

---
+++ DesktopKnowledgeAvalonia/Views/ExaminationWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DesktopKnowledgeAvalonia.Views;
+
+public partial class ExaminationWindow : Window
+{
+    public ExaminationWindow()
+    {
+        InitializeComponent();
+    }
+}
