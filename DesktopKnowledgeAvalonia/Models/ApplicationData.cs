ab225ad71f722f8b8cff606aa9839406e2ed4457
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-06-04 11:06:20 +0800

    增加计时器字段

--- DesktopKnowledgeAvalonia/Models/ApplicationData.cs
+++ DesktopKnowledgeAvalonia/Models/ApplicationData.cs
@@ -7,5 +7,6 @@ namespace DesktopKnowledgeAvalonia.Models;
 public class ApplicationData
 {
     public bool IsInExamination { get; set; } = false;
+    public long? ExaminationTimer  { get; set; } = null;
     public Examination? CurrentExamination { get; set; } = null;
 }
