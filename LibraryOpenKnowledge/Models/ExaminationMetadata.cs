78925200311a1dc26c896a8cb339fdc941e7a862
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-06-06 05:44:54 +0800

    更新数据结构

--- LibraryOpenKnowledge/Models/ExaminationMetadata.cs
+++ LibraryOpenKnowledge/Models/ExaminationMetadata.cs
@@ -13,6 +13,10 @@ public class ExaminationMetadata
     public string? Subject { get; set; } = null; // 试卷学科
     public string? Language { get; set; } = null; // 试卷语言
     public double TotalScore { get; set; } = 0;
+    
+    public long? ExamTime { get; set; } = null; // 推荐考试时间
+    public long? MinimumExamTime { get; set; } = null; // 最小考试时间（考试开始时间为 0 计时的毫秒时间戳，超过此时间后可提前胶卷）
+    public long? MaximumExamTime { get; set; } = null; // 最大考试时间（考试开始时间为 0 计时的毫秒时间戳，超过此时间后无法继续考试）
 
     public ReferenceMaterial[]? ReferenceMaterials { get; set; } = new ReferenceMaterial[] { }; // 试卷参考资料
 }
