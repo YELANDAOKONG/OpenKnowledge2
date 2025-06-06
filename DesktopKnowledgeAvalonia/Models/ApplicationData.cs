dd9fb877598985edd89ae6d431c011fb01ddbc41
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-06-07 03:25:16 +0800

    添加学习模式数据结构

--- DesktopKnowledgeAvalonia/Models/ApplicationData.cs
+++ DesktopKnowledgeAvalonia/Models/ApplicationData.cs
@@ -6,8 +6,16 @@ namespace DesktopKnowledgeAvalonia.Models;
 [Serializable]
 public class ApplicationData
 {
+    // 考试相关
     public bool IsInExamination { get; set; } = false;
     public bool IsTheExaminationStarted { get; set; } = false;
     public long? ExaminationTimer  { get; set; } = null;
     public Examination? CurrentExamination { get; set; } = null;
+    
+    // 学习模式相关
+    public bool IsInStudy { get; set; } = false;
+    public bool IsTheStudyStarted { get; set; } = false;
+    public long? StudyTimer  { get; set; } = null;
+    public Examination? CurrentStudy { get; set; } = null;
+    
 }
